57f77d24501c7e10dcea76e21fd38e20f1540700
Rajyl <33254067+Rajyl@users.noreply.github.com>
2017-12-04 19:52:12 +0800

    Initial Programming Assignment
    Basically tested this one.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,35 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function basically stores a matrix 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## makeVector <- function(x = matrix()) {             ## From the original example sited from the assignment.
+  varInv <- NULL
+  set <- function(y) {
+    x <<- y               ## This is called a superassignment "<<-" https://stat.ethz.ch/pipermail/r-help/2011-April/275905.html
+    varInv <<- NULL       ## The "<<-" and "<-" yelds a different result. ALWAYS REMEMBER!  
+  }
+  get <- function() x
+  setInv <- function(inverse) varInv <<- inverse
+  getInv <- function() varInv
+  list(set = set,
+       get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
-
 ## Write a short comment describing this function
+## This function basically computes the inverse of the matrix from the above function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## cachemean <- function(x, ...) {              ## From the original example sited from the assignment.
+  varInv <- x$getInv()
+  if (!is.null(varInv)) {
+    message("Getting cached data - Inverted Matrix")
+    return(varInv)
+  }
+  mat <- x$get()
+  varInv <- solve(mat, ...)
+  x$setInv(varInv)
+  varInv
 }
